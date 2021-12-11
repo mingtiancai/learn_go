a6ccfad45622834ef9b34bdc3a44e996e73f13e3
mingtiancai <2427373908@qq.com>
2021-12-11 18:26:48 +0800

    add  Dup1

--- chapter1.go
+++ chapter1.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
@@ -41,5 +42,19 @@ func P1_1() {
 func P1_2() {
 	for index, arg := range os.Args[1:] {
 		fmt.Println(index, " ", arg)
+	}
+}
+
+func Dup1() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
 	}
 }
