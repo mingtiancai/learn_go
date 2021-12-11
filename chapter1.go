64a522f5f0831f880fd30e582c8ff484da544572
mingtiancai <2427373908@qq.com>
2021-12-11 21:10:03 +0800

    add Dup2

--- chapter1.go
+++ chapter1.go
@@ -58,3 +58,35 @@ func Dup1() {
 		}
 	}
 }
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
+
+func Dup2() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		//countLines(os.Stdin, counts)
+		return
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Dup2: %v\n", err)
+			}
+
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
