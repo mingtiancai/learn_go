a1a812ece4357d9f333aaa32033fd76a7e51bcb8
mingtiancai <2427373908@qq.com>
2021-12-11 12:11:52 +0800

    add echo

--- chapter1.go
+++ chapter1.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func testPrint() {
 	fmt.Println("test")
+}
+
+func echo() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = ""
+	}
+	fmt.Println(s)
 }
