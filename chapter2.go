dbf859172a8daebd6f47d432afafe5a2d148409b
mingtiancai <2427373908@qq.com>
2021-12-12 22:52:43 +0800

    rename func and variant by chapter

--- chapter2.go
+++ chapter2.go
@@ -6,17 +6,19 @@ import (
 
 const boilingF = 121.0
 
-func UseConst() {
+func C2UseConst() {
 	var f = boilingF
 	var c = (f - 32) * 5 / 9
 	fmt.Printf("boiling point = %g F or %g C\n", f, c)
 
 }
 
-func UsePtr() {
+func C2UsePtr() {
 	x := 1
 	p := &x
 	fmt.Println(*p)
+	fmt.Println(p)
 	*p = 2
 	fmt.Println(x)
+
 }
