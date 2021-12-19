14ba72db96cfc42cdf1645db2415377cc468d64a
mingtiancai <2427373908@qq.com>
2021-12-19 15:31:11 +0800

    add HTMLURL

--- chapter4.go
+++ chapter4.go
@@ -209,6 +209,7 @@ func C4Issue() {
 	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
 	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s createAt: %s\n", item.Number, item.User.Login, item.Title, item.CreateAt)
+		fmt.Printf("#%-5d %9.9s %.55s createAt: %s  url: %s\n",
+			item.Number, item.User.Login, item.Title, item.CreateAt, item.User.HTMLURL)
 	}
 }
