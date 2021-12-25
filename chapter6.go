a2a4d115bc9f05a9cd9b3f4cda0fb99efc31920e
mingtiancai <2427373908@qq.com>
2021-12-25 19:11:15 +0800

    finish chapter6

--- chapter6.go
+++ chapter6.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"math"
+	"sync"
+	"time"
 )
 
 type C6Point struct{ x, y float64 }
@@ -45,4 +48,55 @@ func C6UsePath() {
 		{3, 4},
 	}
 	fmt.Println(perim.Distance())
+}
+
+type C6ColoredPoint struct {
+	C6Point
+	Color color.RGBA
+}
+
+func C6UseC6ColoredPoint() {
+	red := color.RGBA{255, 0, 0, 255}
+	blue := color.RGBA{0, 0, 255, 255}
+	var p = C6ColoredPoint{C6Point{1, 1}, red}
+	var q = C6ColoredPoint{C6Point{5, 4}, blue}
+	fmt.Println(p.Distance(q.C6Point))
+	(&p).C6ScaleBy(2)
+	q.C6ScaleBy(2)
+	fmt.Println(p.Distance(q.C6Point))
+}
+
+type Cache struct {
+	mu      sync.Mutex
+	mapping map[string]string
+}
+
+func UseC6Cache() {
+	cache := Cache{mapping: make(map[string]string)}
+	cache.mu.Lock()
+	fmt.Println("begin sleep! ")
+	time.Sleep(8 * time.Second)
+	cache.mapping["ss"] = "asa"
+	fmt.Println(cache.mapping["ss"])
+	fmt.Println("end sleep! ")
+	cache.mu.Unlock()
+}
+
+type C6Rocket struct {
+	name   string
+	length float64
+}
+
+func (r *C6Rocket) Launch() {
+	fmt.Println("launch")
+	fmt.Println(r.name)
+	fmt.Println(r.length)
+}
+
+func C6UseMethodVar() {
+	r := C6Rocket{"ChangZheng", 17.0}
+	fmt.Println("Begin timing")
+	time.AfterFunc(5*time.Second, r.Launch)
+	time.Sleep(7 * time.Second)
+
 }
